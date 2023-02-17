b9433d5ba5362b43bc56a72fd7cec9d206cb19d9
kimskacegarovs <70476754+kimskacegarovs@users.noreply.github.com>
2023-02-17 16:58:38 +0200

    Add files via upload

---
+++ _next/static/Fqyh9bjabSHrOCxbzBNoa/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e4698bc5c12f82b8.css","static/chunks/pages/index-aecc160f123b79dd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
